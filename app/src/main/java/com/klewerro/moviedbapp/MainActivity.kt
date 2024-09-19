5f2119e2796021f7f4dbcf9fc3b0545daddbb354
Klewerro <pol.szym95@gmail.com>
2024-09-19 15:01:25 +0200

    TopAppBar added in Scaffold. Top margin added to top of the MovieList.

--- app/src/main/java/com/klewerro/moviedbapp/MainActivity.kt
+++ app/src/main/java/com/klewerro/moviedbapp/MainActivity.kt
@@ -7,13 +7,20 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.res.stringResource
+import com.klewerro.moviedbapp.core.presentation.LocalSpacing
 import com.klewerro.moviedbapp.core.ui.theme.MovieDbAppTheme
 import com.klewerro.moviedbapp.movies.presentation.MovieListScreen
 import dagger.hilt.android.AndroidEntryPoint
 
+@OptIn(ExperimentalMaterial3Api::class)
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,9 +28,28 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MovieDbAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text(text = stringResource(R.string.app_name))
+                            },
+                            colors = TopAppBarDefaults.topAppBarColors(
+                                containerColor = MaterialTheme.colorScheme.primary,
+                                titleContentColor = MaterialTheme.colorScheme.onPrimary
+                            ),
+                            navigationIcon = {
+                                // Todo: Implement after adding 2nd screen.
+                            }
+                        )
+                    },
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    val spacing = LocalSpacing.current
                     Box(modifier = Modifier.padding(innerPadding)) {
-                        MovieListScreen(modifier = Modifier.padding(horizontal = 8.dp))
+                        MovieListScreen(
+                            modifier = Modifier.padding(horizontal = spacing.spaceScreen)
+                        )
                     }
                 }
             }
--- movies/src/main/java/com/klewerro/moviedbapp/movies/presentation/MovieListScreen.kt
+++ movies/src/main/java/com/klewerro/moviedbapp/movies/presentation/MovieListScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
@@ -56,6 +57,11 @@ private fun MovieListScreenContent(
             verticalArrangement = Arrangement.spacedBy(spacing.spaceSmall),
             horizontalArrangement = Arrangement.spacedBy(spacing.spaceSmall)
         ) {
+            item(span = {
+                GridItemSpan(this.maxLineSpan)
+            }) {
+                Spacer(modifier = Modifier.padding(top = spacing.spaceScreen))
+            }
             items(
                 moviesPager.itemCount,
                 key = {
